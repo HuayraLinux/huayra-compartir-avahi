4755df2fb4d1f1be62dfc252d36ce59a8e2f3335
Mauro <lavaramano@gmail.com>
2016-02-16 17:14:28 -0300

    Recibe una señal (kill -)SIGTERM para desregistrar(?) al servicio y salir

--- huayra-compartir-avahi.py
+++ huayra-compartir-avahi.py
@@ -1,14 +1,49 @@
-from six.moves import input
-from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf
+#!/usr/bin/env python
+
+import sys
+import time
+import signal
+import socket
+import argparse
 import requests
+import netifaces
+from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf
+
 
 API_PORT = 9919
 USE_API = True
-API_URL_PREFIX = "http://localhost:9919"
+API_URL_PREFIX = "http://localhost:{}".format(API_PORT)
+IS_RUNNING = False
+VERBOSE = False
 
-import socket
-import uuid
-import netifaces
+
+class CompartirListener(object):
+    def __init__(self, verbose=False, *args, **kwargs):
+        self.verbose = verbose
+
+    def remove_service(self, zeroconf, type, name):
+        #info = zeroconf.get_service_info(type, name)
+        if 'huayra-compartir-web-2' in name:
+            machine_id = name.split("__")[1]
+            if self.verbose:
+                print("- baja de servicio ", machine_id)
+
+            if USE_API:
+                requests.delete(API_URL_PREFIX + "/equipos/" + machine_id)
+
+
+    def add_service(self, zeroconf, type, name):
+        if 'huayra-compartir-web-2' in name:
+            info = zeroconf.get_service_info(type, name)
+            machine_id = name.split("__")[1]
+            ip = '.'.join(str(ord(i)) for i in info.address)
+            if self.verbose:
+                print("+ nuevo servicio ", ip, machine_id)
+
+            if USE_API:
+                data = {"host": ip, "id": machine_id}
+                requests.post(API_URL_PREFIX + "/equipos", data)
+
 
 def get_ip_n_macaddr():
     EXCLUDE_IFACES = ['lo', 'lo0']
@@ -24,44 +59,61 @@ def get_ip_n_macaddr():
     return (ip_addr, mac_addr)
 
 
-class MyListener(object):
-
-    def remove_service(self, zeroconf, type, name):
-        #info = zeroconf.get_service_info(type, name)
-        if 'huayra-compartir-web-2' in name:
-            machine_id = name.split("__")[1]
-            print("- baja de servicio ", machine_id)
-
-            if USE_API:
-                requests.delete(API_URL_PREFIX + "/equipos/" + machine_id)
-
-    def add_service(self, zeroconf, type, name):
-        if 'huayra-compartir-web-2' in name:
-            info = zeroconf.get_service_info(type, name)
-            machine_id = name.split("__")[1]
-            ip = '.'.join(str(ord(i)) for i in info.address)
-            print("+ nuevo servicio ", ip, machine_id)
-
-            if USE_API:
-                data = {"host": ip, "id": machine_id}
-                requests.post(API_URL_PREFIX + "/equipos", data)
+def register(zeroconf, info):
+    """
+    Registra un servicio avahi
+    """
+    global IS_RUNNING, VERBOSE
+    if VERBOSE:
+        print("Anunciando servicio como: " + info.name)
+    zeroconf.register_service(info)
+    IS_RUNNING = True
 
 
-ip, mac = get_ip_n_macaddr()
-zeroconf = Zeroconf()
-desc = {}
-name = "huayra-compartir-web-2__" + str(mac) + "__._http._tcp.local."
-info = ServiceInfo("_http._tcp.local.", name, socket.inet_aton(ip), API_PORT, 0, 0, desc)
-
-print("Anunciando servicio como: " + name)
-zeroconf.register_service(info)
-
-
-listener = MyListener()
-browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
-
-try:
-    input("Pulsa ENTER para salir...\n\n")
-finally:
+def unregister(zeroconf, info):
+    """
+    Desregistra(?) un servicio avahi
+    """
+    global browser
     zeroconf.unregister_service(info)
     zeroconf.close()
+
+    browser.cancel()
+
+
+def signal_handler(sig, frame):
+    global zeroconf, info, IS_RUNNING
+
+    unregister(zeroconf, info)
+    IS_RUNNING = False
+
+    time.sleep(5)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose',
+                        action="store_true",
+                        help="Prints status messages")
+
+    args = parser.parse_args()
+    VERBOSE = args.verbose
+
+    ip, mac = get_ip_n_macaddr()
+    zeroconf = Zeroconf()
+    desc = {}
+    name = "huayra-compartir-web-2__" + str(mac) + "__._http._tcp.local."
+    info = ServiceInfo("_http._tcp.local.", name, socket.inet_aton(ip), API_PORT, 0, 0, desc)
+
+    listener = CompartirListener(args.verbose)
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+
+    register(zeroconf, info)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    try:
+        raw_input("<ENTER> para salir" if VERBOSE else "")
+    finally:
+        if IS_RUNNING:
+            unregister(zeroconf, info)
