9fe4265050b71ab737cd59b62b66a90371f9cf71
Mauro <lavaramano@gmail.com>
2016-04-21 14:16:36 -0300

    lockfile + pidfile

--- huayra-compartir-avahi.py
+++ huayra-compartir-avahi.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import time
+import fcntl
 import signal
 import socket
 import argparse
 import requests
+import tempfile
 import netifaces
 from zeroconf import ServiceBrowser, ServiceInfo, Zeroconf
 
+
+__file = os.path.basename(__file__)
 
 API_PORT = 9919
 USE_API = True
 API_URL_PREFIX = "http://localhost:{}".format(API_PORT)
 IS_RUNNING = False
 VERBOSE = False
+LOCKFILE = os.path.join(tempfile.gettempdir(),
+                        "huayra-compartir-avahi-{}.lock".format(os.getuid()))
+PIDFILE = os.path.join(tempfile.gettempdir(),
+                        "huayra-compartir-avahi-{}.pid".format(os.getuid()))
+
 
 
 class CompartirListener(object):
@@ -22,7 +32,6 @@ class CompartirListener(object):
         self.verbose = verbose
 
     def remove_service(self, zeroconf, type, name):
-        #info = zeroconf.get_service_info(type, name)
         if 'huayra-compartir-web-2' in name:
             machine_id = name.split("__")[1]
             if self.verbose:
@@ -91,6 +100,23 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
+def lock():
+    lck = open(LOCKFILE, 'w')
+    fcntl.flock(lck, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+    pf = open(PIDFILE, 'w')
+    pf.write(unicode(os.getpid()))
+    pf.close()
+
+    return lck
+
+
+def release(lck):
+    fcntl.flock(lck, fcntl.LOCK_UN)
+    os.remove(LOCKFILE)
+    os.remove(PIDFILE)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose',
@@ -105,6 +131,7 @@ if __name__ == "__main__":
     desc = {}
     name = "huayra-compartir-web-2__" + str(mac) + "__._http._tcp.local."
     info = ServiceInfo("_http._tcp.local.", name, socket.inet_aton(ip), API_PORT, 0, 0, desc)
+    lck = False
 
     listener = CompartirListener(args.verbose)
     browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
@@ -113,7 +140,14 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, signal_handler)
 
     try:
+        lck = lock()
         raw_input("<ENTER> para salir" if VERBOSE else "")
+    except IOError as e:
+        print "{} is already running!".format(__file)
+        sys.exit(1)
     finally:
+        if lck:
+            release(lck)
+
         if IS_RUNNING:
             unregister(zeroconf, info)
